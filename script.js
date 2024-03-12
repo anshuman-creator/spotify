7ea58760ad190e07d96ef9e9cc64951f327430d8
AN Vishwas <151990112+anshuman-username@users.noreply.github.com>
2024-03-12 13:11:20 +0530

    vzxbzb

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
+function SecToMinAndSec(seconds) {
+  if (typeof seconds !== "number" || seconds < 0) {
+    throw new Error("Input must be a non-negative number.");
+  }
+
+  let roundedSeconds = Math.round(seconds); // Round the seconds to the nearest integer
+  let minutes = Math.floor(roundedSeconds / 60);
+  let remainingSeconds = roundedSeconds % 60;
+  return `${String(minutes).padStart(2, "0")}:${String(
+    remainingSeconds
+  ).padStart(2, "0")}`;
+}
+
+let currentsong = new Audio();
 async function getSongs() {
   let response = await fetch("http://127.0.0.1:3000/songs/");
-  let text = await response.text(); // Await the response text
-  console.log(text);
+  let text = await response.text();
   let div = document.createElement("div");
-  div.innerHTML = text; // Parse the response text into an HTML document
+  div.innerHTML = text;
   let as = div.getElementsByTagName("a");
   console.log(as);
   let songs = [];
@@ -16,39 +29,76 @@ async function getSongs() {
   return songs;
 }
 
+const playMusic = (track, pause = false) => {
+  currentsong.src = "http://127.0.0.1:3000/songs/" + track;
+  if (!pause) {
+    currentsong.play();
+    play.src = "./assets/pause.svg";
+  }
+
+  document.querySelector(".songinfo").innerHTML = track;
+  document.querySelector(".songtime").innerHTML = "00:00 / 00:00";
+};
+
 async function main() {
   let songs = await getSongs();
-  console.log(songs);
-  let songUL = document.querySelector(".songlist").getElementsByTagName("ul")[0];
- for (const song of songs) {
-    songUL.innerHTML = songUL.innerHTML + `
+  playMusic(songs[0], true);
+  let songUL = document
+    .querySelector(".songlist")
+    .getElementsByTagName("ul")[0];
+  for (const song of songs) {
+    songUL.innerHTML =
+      songUL.innerHTML +
+      `
+      
+    
+    <li> 
+      <img class="invert" src="./assets/music.svg" alt="">
+      <div class="info">
+        <div>${song.replaceAll("%20", " ")} </div>
+        <div>Anshu</div>
+      </div>
+      <div class="playnow">
+        <span>Play Now</span>
+        <img class="invert" src="./assets/play.svg" alt="">
+      </div>
+    </li>
     
     
-    
-    
-    <li>
-                <img class="invert" src="./assets/music.svg" alt="">
-                <div class="info">
-                  <div>${song.replaceAll("%20", " ")} </div>
-                  <div>Anshu</div>
-                </div>
-                <div class="playnow">
-                  <span>Play Now</span>
-                  <img class="invert" src="./assets/play.svg" alt="">
-                </div>
-              </li>
-    
-    
-    
-    
-     </li> `
- }
-  var audio = new Audio(songs[0]);
-  audio.play();
-
-  audio.addEventListener("loadeddata", () => {
-    let duration = audio.duration;
-    console.log(audio.duration, audio.currentSrc, audio.currentTime);
+     </li> `;
+  }
+  ////
+  Array.from(
+    document.querySelector(".songlist").getElementsByTagName("li")
+  ).forEach((e) => {
+    e.addEventListener("click", (element) => {
+      playMusic(e.querySelector(".info").firstElementChild.innerHTML);
+    });
   });
+  /////
+
+  play.addEventListener("click", () => {
+    if (currentsong.paused) {
+      currentsong.play();
+      play.src = "./assets/pause.svg";
+    } else {
+      currentsong.pause();
+      play.src = "./assets/play.svg";
+    }
+  });
+  ////
+
+  currentsong.addEventListener("timeupdate", () => {
+    document.querySelector(".songtime").innerHTML = `${SecToMinAndSec(currentsong.currentTime)} / ${SecToMinAndSec(currentsong.duration)}`;
+    document.querySelector('.circle').style.left = (currentsong.currentTime / currentsong.duration) * 100 + "%"
+  });
+
+ document.querySelector('.seekbar').addEventListener("click", e=>{
+  let percent = (e.offsetX/e.target.getBoundingClientRect().width) * 100;
+  document.querySelector('.circle ').style.left = percent + "%"
+  currentsong.currentTime = ((currentsong.duration)*percent)/100
+ })
+ 
+
 }
 main();
